b49ccd2c871576868cbc24a4453b728164e44388
naginagi146 <naginagi146@gmail.com>
2020-08-30 21:58:59 +0900

    複数画像投稿フォーム

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-# from django.views import generic
-# # from .forms import PostCreateForm
-# from .models import Post
+from .forms import ItemCreateForm,ImageFormset
 
+def item_list(request):
+    form = ItemCreateForm(request.POST or None)
+    context = {'form': form}
+    if request.method == 'POST' and form.is_valid():
+        post = form.save(commit=False)
+        image_formset = ImageFormset(request.POST, files=request.FILES, instance=post)  # 増えた
+        if  image_formset.is_valid():
+            post.save()
+            image_formset.save()
+            return redirect('app:index')
 
-# class PostCreate(generic.CreateView):
-#     model = Post
-#     form_class = PostCreateForm
-#     success_url = '/'
-# # Create your views here.
+        # エラーメッセージつきのformsetをテンプレートへ渡すため、contextに格納
+        else:
+            context['image_formset'] = image_formset
+
+    # GETのとき
+    else:
+        # 空のformsetをテンプレートへ渡す
+        context['image_formset'] = ImageFormset()  # 増えた
+
+    return render(request, 'accounts/item_list.html', context)
